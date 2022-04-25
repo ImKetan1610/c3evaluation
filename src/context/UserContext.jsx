fd02a4fa9f5a20bf349de9ee803d10814514766b
Ketan Makode <ketan.makode@gmail.com>
2022-04-25 16:56:42 +0530

    Add files via upload

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,1 +1,24 @@
+import { createContext, useState } from "react";
+import React from 'react'
 
+
+export const UserContext = createContext(undefined)
+
+export function UserProvider ( { children }) {
+    const [total,setTotal] = useState({
+        terminated:0,
+        promoted:0,
+        total_new:0
+    });
+
+    const handlecount= (key) => {
+        var obj = {};
+        obj[key] = total[key]+1;
+        setTotal({...total,...obj})
+    }
+    return (
+        <UserContext.Provider value={{total,handlecount}}>
+            { children }
+        </UserContext.Provider>
+    )
+}
